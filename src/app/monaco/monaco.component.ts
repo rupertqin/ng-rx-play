7406420476324d60d1434f98f15ae808c24c7ac2
qinx <rupert.qing@gmail.com>
2021-12-13 11:07:06 +0800

    fix: monaco setting

--- src/app/monaco/monaco.component.ts
+++ src/app/monaco/monaco.component.ts
@@ -1,5 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+const code: string = `
+import { Injectable } from '@angular/core';
+import { Subject, Observable, Subscriber } from 'rxjs';
+
+@Injectable()
+export class MissionService {
+
+  // Observable string sources
+  private missionAnnouncedSource: Subscriber<string>[] = [];
+  private missionConfirmedSource = new Subscriber<string>();
+
+  missionAnnounced$: Observable<string>;
+  missionConfirmed$: Observable<string>;
+
+
+  constructor() {
+    // Observable string streams
+    this.missionAnnounced$ = new Observable(subscriber => {
+      this.missionAnnouncedSource.push(subscriber)
+    })
+    this.missionConfirmed$ = new Observable(subscriber => {
+      this.missionConfirmedSource = subscriber
+    })
+  }
+
+  // Service message commands
+  announceMission(mission: string) {
+    this.missionAnnouncedSource.forEach(ss => ss.next(mission));
+  }
+
+  confirmMission(astronaut: string) {
+    this.missionConfirmedSource.next(astronaut);
+  }
+}
+`;
+
 @Component({
   selector: 'app-monaco',
   templateUrl: './monaco.component.html',
@@ -8,8 +44,11 @@ import { Component, OnInit } from '@angular/core';
 export class MonacoComponent implements OnInit {
 
   constructor() { }
-  editorOptions = {theme: 'vs-dark', language: 'javascript'};
-  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
+  editorOptions = {
+    theme: 'vs-dark', 
+    language: 'javascript'
+  };
+  code: string = code;
 
 
   ngOnInit(): void {
