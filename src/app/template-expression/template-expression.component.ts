11ca226a303ca74f234caf313b7d07a55d46e5de
qinx <rupert.qing@gmail.com>
2022-01-20 14:31:21 +0800

    detectChanges

--- src/app/template-expression/template-expression.component.ts
+++ src/app/template-expression/template-expression.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, AfterViewInit, ChangeDetectionStrategy, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-template-expression',
@@ -6,12 +6,44 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./template-expression.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TemplateExpressionComponent implements OnInit {
-  constructor() { }
+export class TemplateExpressionComponent implements OnChanges, AfterViewInit {
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    // 这里能触发变化
+    // this.familyName = 'Birkin'
+
+    // setTimeout(() => {
+    //   // 这里不能能触发变化
+    //   this.familyName = 'Birkin latter'
+    // }, 3000)
+  }
   familyName = ''
   
-
   ngOnInit(): void {
+    // 这里能触发变化
+    // this.familyName = 'Birkin'
+
+    // setTimeout(() => {
+    //   // 这里不能能触发变化
+    //   this.familyName = 'Birkin latter'
+    // }, 3000)
+  }
+
+  ngAfterViewInit(): void {
+    // 这里不能触发变化
+    // ChangeDetectionStrategy.Default: 同步mutate 会报错： ExpressionChangedAfterItHasBeenCheckedError
+    // this.familyName = 'Birkin view'
+
+    setTimeout(() => {
+      // 这里不能能触发变化
+      this.familyName = 'Birkin view latter'
+      this.changeDetectorRef.detectChanges()
+    }, 3000)
+      
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    debugger
+    console.log('changes: ', changes.familyName)
   }
 
   changeName() {
