7ecb63d713a3d7f974a1571bc3ddbd8d0db7b832
qinx <rupert.qing@gmail.com>
2022-01-21 17:57:23 +0800

    feat: shared module with export module

--- src/app/prism/prism.module.ts
+++ src/app/prism/prism.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PrismModule } from '../prism/prism.module'
+import { PrismComponent } from './prism.component'
 
 @NgModule({
-  declarations: [],
+  declarations: [PrismComponent],
   imports: [
     CommonModule,
   ],
-  exports: [PrismModule],
+  exports: [PrismComponent],
 })
-export class SharedModule { }
+export class PrismModule { }
